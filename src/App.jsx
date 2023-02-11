b94147e271fc023c4815123a9ffbb6a7b1822d42
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-02-11 22:55:40 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import { Routes, Route } from "react-router-dom";
 import { Home, Movie } from "./pages";
 
 function App() {
-
   const [results, setResults] = useState({});
 
   return (
-    <Routes>
-      <Route exact path="/" element={<Home results={results} setResults={setResults}/>} />
-      <Route exact path="/movie/:slug" element={<Movie results={results} />} />
-    </Routes>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Home results={results} setResults={setResults} />}
+        />
+        <Route
+          exact
+          path="/movie/:slug"
+          element={<Movie results={results} />}
+        />
+        <Route exact path="/favourites" />
+      </Routes>
   );
 }
 
